22adb85db952bc6f250467bc05795cde447c4c81
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2020-09-15 14:48:34 -0300

    Borrado de enlace

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
         <p>
           Bienvenidos al Webinar de Git + GitHub
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
